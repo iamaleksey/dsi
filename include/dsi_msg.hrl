a6f95bfee9ca603ed79995e4e92c099ce330194c
Aleksey Yeschenko <aleksey@yeschenko.com>
2010-09-28 20:31:54 +0300

    narrow dsi_hdr record spec

--- include/dsi_msg.hrl
+++ include/dsi_msg.hrl
@@ -1,9 +1,9 @@
--record(dsi_hdr, {type         :: non_neg_integer(),   % type of message.
-                  id       = 0 :: non_neg_integer(),   % module instatiation.
-                  instance = 0 :: non_neg_integer(),   % module instance.
-                  src      = 0 :: non_neg_integer(),   % sending module ID.
-                  dst          :: non_neg_integer(),   % destination module ID.
-                  status   = 0 :: non_neg_integer(),   % returned status.
-                  err_info = 0 :: non_neg_integer()}). % status infromation.
+-record(dsi_hdr, {type         :: 0..65535,        % type of message.
+                  id       = 0 :: 0..65535,        % module instatiation.
+                  instance = 0 :: 0..65535,        % module instance.
+                  src      = 0 :: 0..255,          % sending module ID.
+                  dst          :: 0..255,          % destination module ID.
+                  status   = 0 :: 0..255,          % returned status.
+                  err_info = 0 :: 0..4294967295}). % status infromation.
 
 -record(dsi_msg, {hdr :: #dsi_hdr{}, body :: binary()}).
