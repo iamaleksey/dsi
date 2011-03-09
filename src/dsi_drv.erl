2f2cda9e04b6b6f0320cdfb29bc0540f19701faa
Aleksey Yeschenko <aleksey@yeschenko.com>
2011-03-09 14:01:54 +0200

    remove redundant whitespace

--- src/dsi_drv.erl
+++ src/dsi_drv.erl
@@ -18,15 +18,11 @@
 %%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 %%% THE SOFTWARE.
 
-
 -module(dsi_drv).
-
 
 -behaviour(gen_server).
 
-
 -include("dsi_msg.hrl").
-
 
 %% API exports
 -export([start_link/0,
@@ -38,7 +34,6 @@
          unlink/1,
          is_congested/2]).
 
-
 %% gen_server exports
 -export([init/1,
          terminate/2,
@@ -47,9 +42,7 @@
          handle_info/2,
          code_change/3]).
 
-
 -type mod_id()  :: non_neg_integer().
-
 
 -define(APP_NAME, dsi).
 -define(DRV_NAME, "dsi_drv").
@@ -64,67 +57,55 @@
 -define(DSI_STANDARD, 0).
 -define(DSI_LONG,     1).
 
-
 -record(st, {port :: port()}).
-
 
 %% -------------------------------------------------------------------------
 %% API
 %% -------------------------------------------------------------------------
-
 
 -spec start_link/0 :: () -> {'ok', pid()} | {'error', any()}.
 
 start_link() ->
     gen_server:start_link(?MODULE, [], []).
 
-
 -spec stop/1 :: (pid()) -> 'ok'.
 
 stop(Pid) ->
     gen_server:cast(Pid, stop).
-
 
 -spec send/3 :: (pid(), mod_id(), dsi_msg()) -> 'ok' | 'error'.
 
 send(Pid, ModId, Msg) ->
     gen_server:call(Pid, {send, ModId, Msg}, infinity).
 
-
 -spec recv/2 :: (pid(), mod_id()) -> {'ok', dsi_msg()} | 'error'.
 
 recv(Pid, ModId) ->
     gen_server:call(Pid, {recv, ModId}, infinity).
-
 
 -spec grab/2 :: (pid(), mod_id()) -> {'ok', dsi_msg()} | 'empty'.
 
 grab(Pid, ModId) ->
     gen_server:call(Pid, {grab, ModId}, infinity).
 
-
 -spec link/1 :: (pid()) -> boolean().
 
 link(Pid) ->
     gen_server:call(Pid, link, infinity).
-
 
 -spec unlink/1 :: (pid()) -> 'ok'.
 
 unlink(Pid) ->
     gen_server:call(Pid, unlink, infinity).
 
-
 -spec is_congested/2 :: (pid(), 'standard' | 'long') -> boolean().
 
 is_congested(Pid, Type) ->
     gen_server:call(Pid, {is_congested, Type}, infinity).
 
-
 %% -------------------------------------------------------------------------
 %% gen_server callback functions
 %% -------------------------------------------------------------------------
-
 
 init([]) ->
     case erl_ddll:load(code:priv_dir(?APP_NAME), ?DRV_NAME) of
@@ -135,10 +116,8 @@ init([]) ->
             {stop, Reason}
     end.
 
-
 terminate(_Reason, St) ->
     port_close(St#st.port).
-
 
 handle_call({send, ModId, Msg}, _From, St) ->
     #dsi_msg{
@@ -155,26 +134,21 @@ handle_call({send, ModId, Msg}, _From, St) ->
     port_command(St#st.port, [?DSI_SEND, ModId, Hdr, Body]),
     {reply, port_reply(), St};
 
-
 handle_call({recv, ModId}, _From, St) ->
     port_command(St#st.port, [?DSI_RECV, ModId]),
     {reply, port_reply(), St};
-
 
 handle_call({grab, ModId}, _From, St) ->
     port_command(St#st.port, [?DSI_GRAB, ModId]),
     {reply, port_reply(), St};
 
-
 handle_call(link, _From, St) ->
     port_command(St#st.port, [?DSI_LINK]),
     {reply, port_reply(), St};
 
-
 handle_call(unlink, _From, St) ->
     port_command(St#st.port, [?DSI_UNLINK]),
     {reply, port_reply(), St};
-
 
 handle_call({is_congested, Type}, _From, St) ->
     Byte =
@@ -185,31 +159,24 @@ handle_call({is_congested, Type}, _From, St) ->
     port_command(St#st.port, [?DSI_IS_CONGESTED, Byte]),
     {reply, port_reply(), St};
 
-
 handle_call(Request, _From, St) ->
     {stop, {unexpected_call, Request}, St}.
-
 
 handle_cast(stop, St) ->
     {stop, normal, St};
 
-
 handle_cast(Request, St) ->
     {stop, {unexpected_cast, Request}, St}.
-
 
 handle_info(Info, St) ->
     {stop, {unexpected_info, Info}, St}.
 
-
 code_change(_OldVsn, St, _Extra) ->
     {ok, St}.
-
 
 %% -------------------------------------------------------------------------
 %% private functions
 %% -------------------------------------------------------------------------
-
 
 port_reply() ->
     receive
--- test/dsi_drv_tests.erl
+++ test/dsi_drv_tests.erl
@@ -18,13 +18,10 @@
 %%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 %%% THE SOFTWARE.
 
-
 -module(dsi_drv_tests).
-
 
 -include_lib("eunit/include/eunit.hrl").
 -include("dsi_msg.hrl").
-
 
 -define(MOD_ID, 16#2d).
 -define(MSG,
@@ -38,7 +35,6 @@
         err_info = 4294967295,
         body     = <<1, 2, 3, 4>>
     }).
-
 
 dsi_drv_test_() ->
     {setup,
@@ -60,34 +56,27 @@ dsi_drv_test_() ->
         end
     }.
 
-
 test_send(Pid) ->
     ?assertEqual(ok, dsi_drv:send(Pid, ?MOD_ID, ?MSG)),
     dsi_drv:recv(Pid, ?MOD_ID). % empty the queue.
-
 
 test_recv(Pid) ->
     dsi_drv:send(Pid, ?MOD_ID, ?MSG),
     ?assertEqual({ok, ?MSG}, dsi_drv:recv(Pid, ?MOD_ID)).
 
-
 test_grab_non_empty(Pid) ->
     dsi_drv:send(Pid, ?MOD_ID, ?MSG),
     ?assertEqual({ok, ?MSG}, dsi_drv:grab(Pid, ?MOD_ID)).
 
-
 test_grab_empty(Pid) ->
     ?assertEqual(empty, dsi_drv:grab(Pid, ?MOD_ID)).
-
 
 test_is_congested(Pid) ->
     ?assertEqual(false, dsi_drv:is_congested(Pid, standard)),
     ?assertEqual(false, dsi_drv:is_congested(Pid, long)).
 
-
 test_link(Pid) ->
     ?assertEqual(true, dsi_drv:link(Pid)).
-
 
 test_unlink(Pid) ->
     ?assertEqual(ok, dsi_drv:unlink(Pid)).
